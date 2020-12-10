b4092f369b1a789c3520498824410045dfdb6e50
Andrade <igor.andrade@capgemini.com>
2020-12-10 15:01:58 -0300

    add formulario cliente

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { ProfessorComponent } from './professor/professor.component';
 import { ClienteComponent } from './cliente/cliente.component';
 import { FornecedorComponent } from './fornecedor/fornecedor.component';
 import { AppRoutingModule } from './app-routing.module';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,8 @@ import { AppRoutingModule } from './app-routing.module';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/cliente/cliente.component.ts
+++ src/app/cliente/cliente.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-cliente',
@@ -10,11 +11,12 @@ export class ClienteComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
-    type MyArrayType = Array<{id:number, text:string}>
+
+    /*type MyArrayType = Array<{id:number, text:string}>
 
     var lista:MyArrayType = [{
       id:1, text:'Sentence1'
-    }];
+    }];*/
 
     /*for(var item of lista){ //for off loop
       console.log(item.id)
@@ -22,21 +24,32 @@ export class ClienteComponent implements OnInit {
       console.log(item)
     }*/
 
+    /*
     //for usando map(aerofunction)
     lista.map(item => {
       console.log(item.id);
       console.log(item.text);
       console.log(item);
-    });
-
-    //for usando map(aerofunction)
+    });*/
+/*
+    //for usando forEach(aerofunction)
     lista.forEach(item => {
       console.log(item.id);
       console.log(item.text);
       console.log(item);
-    });
-
-
+    });*/
   }
+    onSubmit(form:NgForm)
+    {
+      console.log(form);
+    }  
 
+    printNome(nome:string){
+      console.log(nome);
+    }
+
+    onSubmitProd(form:NgForm)
+    {
+      console.log(form.value);
+    }  
 }
